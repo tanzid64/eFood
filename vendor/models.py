097b9c839138cac944560508d0e3e0c3802feafa
tanzid64 <tanzid@inbox.ru>
2024-07-29 11:49:02 +0600

    Email functionality added on vendor approved or disapproved by admin

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from vendor.utils import send_notification
 
 # Create your models here.
 
@@ -14,3 +15,21 @@ class Vendor(models.Model):
 
   def __str__(self):
     return self.vendor_name
+  
+  def save(self, *args, **kwargs):
+    if self.pk is not None:
+      orig = Vendor.objects.get(pk=self.pk)
+      if orig.is_approved != self.is_approved:
+        context = {
+          'user': self.user,
+          'is_approved': self.is_approved
+        }
+        template = 'vendor/emails/admin_vendor_approval.html'
+        if self.is_approved == True:
+          mail_subject = 'Congratulations! Your restaurant has been approved.'
+          
+          send_notification(template, mail_subject, context)
+        else:
+          mail_subject = 'We are sorry! Your restaurant has been disapproved.'
+          send_notification(template, mail_subject, context)
+    return super(Vendor, self).save(*args, **kwargs)
---
+++ vendor/utils.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+
+def send_notification(template, email_subject, context):
+  from_email = settings.DEFAULT_FROM_EMAIL
+  email_body = render_to_string(template, context)
+  to_email = context['user'].email
+  email = EmailMessage(
+      subject=email_subject,
+      body=email_body,
+      from_email=from_email,
+      to=[to_email],
+    )
+  email.send()
