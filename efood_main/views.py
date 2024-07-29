bef245715a71c08d7f3b24dcd8088605e7088b1f
tanzid64 <tanzid@inbox.ru>
2024-07-30 01:28:32 +0600

    homepage updated..

--- efood_main/views.py
+++ efood_main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from vendor.models import Vendor
 
 def home(request):
-  return render(request, 'efood_main/home.html')
+  vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+  context = {
+    'vendors': vendors
+  }
+  return render(request, 'efood_main/home.html', context)
