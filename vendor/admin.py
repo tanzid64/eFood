65df6d14b92d93caea4f226f833b09a66110c875
tanzid64 <tanzid@inbox.ru>
2024-07-30 02:08:45 +0600

    updated: in vendor model slug field added..

--- vendor/admin.py
+++ vendor/admin.py
@@ -4,6 +4,7 @@ from vendor.models import Vendor
 # Register your models here.
 
 class VendorAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'vendor_slug':('vendor_name',)}
   list_display = ['user', 'vendor_name', 'is_approved', 'created_at']
   list_display_links = ['user', 'vendor_name']
   list_editable = ['is_approved']
